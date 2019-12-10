c8fc80045a8571ec0f5d0d1b56b0562cc1fb14a5
barisicm <barisic.m@outlook.com>
2019-12-10 22:11:06 +0100

    Added browsersync reload on document changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,7 @@
 var gulp = require('gulp');
-// Define sass compiler
-var sass = require('gulp-dart-sass');
 // Define browser sync
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-// Define Css Minifyer
-var cleanCSS = require('gulp-clean-css');
-// Gulp rename plugin
-var rename = require("gulp-rename");
-var sourcemaps = require("gulp-sourcemaps");
-
 
 gulp.task('serve', function() {
     browserSync.init({
@@ -20,22 +12,6 @@ gulp.task('serve', function() {
     });
 
     gulp.watch('*.html').on('change', reload);
-    gulp.watch('./css/main.min.css').on('change', reload);
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
-});
-
-
-gulp.task('sass', function() {
-    return gulp.src('./SASS/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./css/'));
-});
-
-
-gulp.task('sass:watch', function() {
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('css/*.css').on('change', reload);
 });
 
