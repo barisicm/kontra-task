f07626425be81aa03444f37d0f29d1f62b934de3
Marin Barišić <marin.barisic.external@t.ht.hr>
2019-12-14 20:17:34 +0100

    Added gulp build processes for js and css.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ var gulp = require('gulp');
 // Define browser sync
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css');
 
 gulp.task('serve', function() {
     browserSync.init({
-        open: false,
         server: {
-            baseDir:'./'
+            baseDir:'./dist'
+        },
+        middleware: function(req, res, next) {
+            res.setHeader("Content-Encoding", "gzip");
+            next();
         }
     });
 
@@ -15,3 +21,25 @@ gulp.task('serve', function() {
     gulp.watch('css/*.css').on('change', reload);
 });
 
+// Gulp task for bundling JavaScript files
+gulp.task('pack-js', function() {
+    return gulp.src(['js/*.js'])
+        .pipe(concat('bundle.js'))
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+//Gulp task for bundling CSS files
+gulp.task('pack-css', function () {    
+    return gulp.src('css/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', gulp.series('pack-css', 'pack-js'));
